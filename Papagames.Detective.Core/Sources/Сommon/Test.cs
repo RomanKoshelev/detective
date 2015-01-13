deeaef01fcc47dc1466d24180fb8b392ee6f6fcd
Roman Koshelev <romakosh@gmail.com>
2015-01-14 02:39:45 +0300

    xoxo

---
+++ Papagames.Detective.Core/Sources/Сommon/Test.cs
@@ -0,0 +1,17 @@
+﻿using MoreLinq;
+
+namespace Papagames.Detective.Core
+{
+    public class Test
+    {
+        public static string HelloWorld()
+        {
+            var list = new string[] {"Hello", "World"};
+            var res = "";
+            
+            list.ForEach(s=>res=res+" " + s);
+
+            return res;
+        }
+    }
+}
