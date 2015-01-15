63aeebe3d58ea4fe4da10fbb28c2f97f831bd180
Roman Koshelev <romakosh@gmail.com>
2015-01-15 19:55:04 +0300

    asp game page

---
+++ Projects/Papagames.Detective.App.Web/Controllers/GameController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Papagames.Detective.App.Web.Models;
+
+namespace Papagames.Detective.App.Web.Controllers
+{
+    public class GameController : Controller
+    {
+        public ActionResult Index()
+        {
+            var _game = new Game();
+
+            return View(_game);
+        }
+    }
+}
---
+++ Projects/Papagames.Detective.App.Web/Models/Game.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Papagames.Detective.App.Web.Models
+{
+    public class Game
+    {
+        public string Name = "Detective Game";
+    }
+}
