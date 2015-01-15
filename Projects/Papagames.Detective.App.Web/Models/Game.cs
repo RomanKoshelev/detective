02e1bfacaae105f96461a6a41eb828c04c7fa2e3
Roman Koshelev <romakosh@gmail.com>
2015-01-15 22:13:21 +0300

    ref:  +common

--- Projects/Papagames.Detective.App.Web/Models/Game.cs
+++ Projects/Papagames.Detective.App.Web/Models/Game.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.CodeDom;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,5 +9,11 @@ namespace Papagames.Detective.App.Web.Models
     public class Game
     {
         public string Name = "Detective Game";
+        private Core.Game _process;
+
+        public Game()
+        {
+            //_process = new Core.Game(8, 3);
+        }
     }
 }
