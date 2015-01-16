19b1d42c82e795e1e36730b2413669fc3848a176
Roman Koshelev <romakosh@gmail.com>
2015-01-16 15:35:23 +0300

    ref: ren Common -> Utils

--- Projects/Papagames.Detective.Utils/Properties/AssemblyInfo.cs
+++ Projects/Papagames.Detective.Utils/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Papagames.Detective.Common")]
+[assembly: AssemblyTitle("Papagames.Detective.Utils")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Papagames.Detective.Common")]
+[assembly: AssemblyProduct("Papagames.Detective.Utils")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
