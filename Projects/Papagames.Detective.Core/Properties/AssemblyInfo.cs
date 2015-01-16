13e08d676698763e370d43e15a625c42d86d156f
Roman Koshelev <romakosh@gmail.com>
2015-01-16 15:12:49 +0300

    ref: ren prj Papagames.Detective.Core.Game

--- Projects/Papagames.Detective.Core/Properties/AssemblyInfo.cs
+++ Projects/Papagames.Detective.Core/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Papagames.Detective.Core")]
+[assembly: AssemblyTitle("Papagames.Detective.Core.Game")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Papagames.Detective.Core")]
+[assembly: AssemblyProduct("Papagames.Detective.Core.Game")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
