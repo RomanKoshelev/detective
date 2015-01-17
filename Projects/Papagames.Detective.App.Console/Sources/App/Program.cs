ba63e75d73179c561b106914b1911d04b0cf4091
Roman Koshelev <romakosh@gmail.com>
2015-01-17 20:55:36 +0300

    ren Schem[a]

--- Projects/Papagames.Detective.App.Console/Sources/App/Program.cs
+++ Projects/Papagames.Detective.App.Console/Sources/App/Program.cs
@@ -10,7 +10,7 @@ namespace Papagames.Detective.App.Console
         {
             try
             {
-                PlayGame(Schema.WorldId.Simpsons, 8, 3, autoMode: true);
+                PlayGame(Schema.WorldId.Simpsons, 8, 3, autoMode: false);
             }
             catch (Exception e)
             {
--- Projects/Papagames.Detective.App.Web/Controllers/HomeController.cs
+++ Projects/Papagames.Detective.App.Web/Controllers/HomeController.cs
@@ -14,6 +14,5 @@ namespace Papagames.Detective.App.Web.Controllers
             player.LoadCases();
             return View(player);
         }
-
     }
 }
