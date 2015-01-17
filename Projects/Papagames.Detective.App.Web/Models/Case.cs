7e95bfe0a6a36216a32f287e99dc78618dc7e13e
Roman Koshelev <romakosh@gmail.com>
2015-01-17 19:42:57 +0300

    add: Case Model

---
+++ Projects/Papagames.Detective.App.Web/Models/Case.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Papagames.Detective.App.Web.Models
+{
+    public class Case
+    {
+        public string WorldName;
+        
+        public IList<string> ActiveMembers;
+        public IList<string> Victims;
+
+        public int MemberNum;
+        public int MurdererNum;
+     }
+}
