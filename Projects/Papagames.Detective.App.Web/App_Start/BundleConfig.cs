689c843beb6ac379727d5347ee9dea58a5e3a7c4
Roman Koshelev <romakosh@gmail.com>
2015-01-30 14:46:49 +0300

    DET-52 move layout to ~/Views/Shared/Layouts/Default.cshtml

--- Projects/Papagames.Detective.App.Web/App_Start/BundleConfig.cs
+++ Projects/Papagames.Detective.App.Web/App_Start/BundleConfig.cs
@@ -7,8 +7,14 @@ namespace Papagames.Detective.App.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
-            bundles.Add(new StyleBundle("~/Content/css").Include("~/Content/site.css"));
-            bundles.Add(new StyleBundle("~/Content/Themes/Table/css").Include("~/Content/Themes/Table/site.css"));
+            bundles.Add(new StyleBundle("~/Content/css")
+                .Include("~/Content/Themes/Default/site.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/Themes/Table/css")
+                .Include("~/Content/Themes/Table/site.css"));
+            
+            bundles.Add(new StyleBundle("~/Content/Themes/Default/css")
+                .Include("~/Content/Themes/Default/site.css"));
         }
     }
 }
