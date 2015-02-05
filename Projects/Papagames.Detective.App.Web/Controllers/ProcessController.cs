ddd5c722e5ed5b555fb7065461bfa85adddce2f6
Roman Koshelev <romakosh@gmail.com>
2015-02-05 22:32:39 +0300

    DET-64 Ask action works

--- Projects/Papagames.Detective.App.Web/Controllers/ProcessController.cs
+++ Projects/Papagames.Detective.App.Web/Controllers/ProcessController.cs
@@ -2,6 +2,7 @@
 using Papagames.Detective.App.Web.Models;
 using Papagames.Detective.Core.Game;
 using Papagames.Detective.Utils;
+using WebGrease.Css.Extensions;
 
 namespace Papagames.Detective.App.Web.Controllers
 {
@@ -24,24 +25,32 @@ namespace Papagames.Detective.App.Web.Controllers
             var processId = (Identifiable<int, Process>.Identifier) id;
             if (actionType != null)
             {
-                Schema.ExecuteProcess(processId, (Process.UserAction.ActionType)actionType, actionParams);
+                Schema.ExecuteProcess(processId, (Process.UserAction.ActionType) actionType, actionParams);
                 return RedirectToAction("Play", "Process", new {id = processId});
             }
             return View(new ProcessModel(processId));
         }
 
         // >> Precess > Controller > ClassicPlay
-        public ActionResult ClassicPlay(int id, int? face, int? card, int? action, params int[] args)
+        public ActionResult ClassicPlay(int id,
+            int? face,
+            int? card,
+            int? actionType,
+            int? respondent,
+            int? subject)
         {
-            var processId = (Identifiable<int, Process>.Identifier)id;
-            if (action != null)
+            var processId = (Identifiable<int, Process>.Identifier) id;
+            if (actionType != null)
             {
-                Schema.ExecuteProcess(processId, (Process.UserAction.ActionType)action, args);
-                return RedirectToAction("ClassicPlay", "Process", new { id = processId });
+                Schema.ExecuteProcess(processId, (Process.UserAction.ActionType)actionType,
+                    new[] {respondent ?? 0, subject ?? 0});
+                return RedirectToAction("ClassicPlay", "Process", new {id = processId});
             }
-            
+
             ViewBag.Face = face;
             ViewBag.Card = card;
+            ViewBag.Respondent = respondent;
+            ViewBag.Subject = subject;
 
             return View(new ProcessModel(processId));
         }
