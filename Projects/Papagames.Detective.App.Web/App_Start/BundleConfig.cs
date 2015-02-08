2dbd3ca33602adebbde92b008d4b3aa39409aa2d
Roman Koshelev <romakosh@gmail.com>
2015-02-09 01:18:46 +0300

    DET-65 improves

--- Projects/Papagames.Detective.App.Web/App_Start/BundleConfig.cs
+++ Projects/Papagames.Detective.App.Web/App_Start/BundleConfig.cs
@@ -7,12 +7,16 @@ namespace Papagames.Detective.App.Web
         // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
         public static void RegisterBundles(BundleCollection bundles)
         {
+            // CSS
             bundles.Add(new StyleBundle("~/Content/css")
                 .Include("~/Content/Themes/Default/site.css"));
-            
+
+            // Classic CSS
             bundles.Add(new StyleBundle("~/Content/Themes/Classic/css")
-                .Include("~/Content/Themes/Classic/site.css"));
-            
+                .Include("~/Content/Themes/Classic/Site.css")
+                .Include("~/Content/Themes/Classic/GameBoard.css"));
+
+            // Default CSS
             bundles.Add(new StyleBundle("~/Content/Themes/Default/css")
                 .Include("~/Content/Themes/Default/site.css"));
         }
