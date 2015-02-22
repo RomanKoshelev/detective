6fd9ba9cdccc9b2fa6bc9cf54be6bc928c75606b
Roman Koshelev <romakosh@gmail.com>
2015-02-22 16:23:17 +0300

    CN-50 SiteController

---
+++ Projects/Crimenuts.App.Web/Controllers/SiteController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using Crimenuts.App.Web.Models;
+using Crimenuts.Core.Game;
+using Crimenuts.Utils;
+
+namespace Crimenuts.App.Web.Controllers
+{
+    public class CaseController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new SchemaModel());
+        }
+
+        public ActionResult Info(int id)
+        {
+            var caseId = (Identifiable<int, Case>.Identifier)id;
+            return View(new CaseModel(caseId));
+        }
+
+        public ActionResult Run(int id)
+        {
+            var caseId = (Identifiable<int, Case>.Identifier)id;
+            var processId = SchemaModel.RunNewProcess(caseId, State.Questioning);
+
+            return RedirectToAction("Play", "Process", new { id = processId });
+        }
+
+        public ActionResult Relations(int id)
+        {
+            var caseId = (Identifiable<int, Case>.Identifier)id;
+            return View(new CaseModel(caseId));
+        }
+    }
+}
