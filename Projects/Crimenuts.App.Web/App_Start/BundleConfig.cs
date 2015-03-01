a4801469d9d60b96f27ea0ca1073f7683fd6dfa5
Roman Koshelev <romakosh@gmail.com>
2015-03-01 23:52:59 +0300

    CN-86 site struct done

--- Projects/Crimenuts.App.Web/App_Start/BundleConfig.cs
+++ Projects/Crimenuts.App.Web/App_Start/BundleConfig.cs
@@ -4,7 +4,7 @@ namespace Crimenuts.App.Web
 {
     public static class BundleConfig
     {
-        // For more information on Bundling, visit http://go.microsoft.com/fwlink/?LinkId=254725
+        // >> BundleConfig *
         public static void RegisterBundles(BundleCollection bundles)
         {
             // CSS
@@ -15,6 +15,7 @@ namespace Crimenuts.App.Web
             bundles.Add(new StyleBundle("~/Content/Themes/Classic/css")
                 .Include("~/Content/Themes/Classic/Common.css")
                 .Include("~/Content/Themes/Classic/Layout.css")
+                .Include("~/Content/Themes/Classic/Home.css")
                 .Include("~/Content/Themes/Classic/Room.css")
                 .Include("~/Content/Themes/Classic/Room.Members.css")
                 .Include("~/Content/Themes/Classic/Room.Board.css"));
