53b4acc7b5a30f2fe6572abad6f7eaa699567d38
Roman Koshelev <romakosh@gmail.com>
2015-03-02 00:32:14 +0300

    CN-61 process new design

--- Projects/Crimenuts.App.Web/App_Start/BundleConfig.cs
+++ Projects/Crimenuts.App.Web/App_Start/BundleConfig.cs
@@ -4,7 +4,7 @@ namespace Crimenuts.App.Web
 {
     public static class BundleConfig
     {
-        // >> BundleConfig *
+        // >> | BundleConfig *
         public static void RegisterBundles(BundleCollection bundles)
         {
             // CSS
@@ -15,7 +15,7 @@ namespace Crimenuts.App.Web
             bundles.Add(new StyleBundle("~/Content/Themes/Classic/css")
                 .Include("~/Content/Themes/Classic/Common.css")
                 .Include("~/Content/Themes/Classic/Layout.css")
-                .Include("~/Content/Themes/Classic/Home.css")
+                .Include("~/Content/Themes/Classic/Info.css")
                 .Include("~/Content/Themes/Classic/Room.css")
                 .Include("~/Content/Themes/Classic/Room.Members.css")
                 .Include("~/Content/Themes/Classic/Room.Board.css"));
