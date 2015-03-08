35fef8355b7205b2fb50a173f18274a192d3f14a
Roman Koshelev <romakosh@gmail.com>
2015-03-08 15:27:49 +0300

    cn-94 ImageController

---
+++ Projects/Crimenuts.App.Web/Controllers/ImageController.cs
@@ -0,0 +1,20 @@
+﻿
+using System;
+using System.IO;
+using System.Web.Mvc;
+
+namespace Crimenuts.App.Web.Controllers
+{
+    public class ImageController : Controller
+    {
+        // Code: ImageController.World
+        public ActionResult World(int id)
+        {
+            // No need to dispose the stream, MVC does it for you
+            var path = Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "App_Data", @"Worlds\Simpsons\Images\simpsons.png");
+            var stream = new FileStream(path, FileMode.Open);
+            var result = new FileStreamResult(stream, "image/png") { FileDownloadName = "image.png" };
+            return result;
+        }
+    }
+}
