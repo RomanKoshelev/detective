3faf4779497757556b4d4d2de0638e7e899b1e55
Roman Koshelev <romakosh@gmail.com>
2015-03-29 20:25:14 +0300

    CN-105 Project Research.Signalr.Typescript

---
+++ Projects/Research.Signalr.Typescript/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿// Crimenuts (c) 2015 Crocodev
+// Research.Signalr.Typescript
+// HomeController.cs
+// Roman, 2015-03-29 7:27 PM
+
+using System.Web.Mvc;
+
+namespace Research.Signalr.Typescript.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+
+        public ActionResult Chat()
+        {
+            return View();
+        }
+    }
+}
---
+++ Projects/Research.Signalr.Typescript/Hubs/ChatHub.cs
@@ -0,0 +1,18 @@
+﻿// Crimenuts (c) 2015 Crocodev
+// Research.Signalr.Typescript
+// ChatHub.cs
+// Roman, 2015-03-29 7:11 PM
+
+using Microsoft.AspNet.SignalR;
+
+namespace Research.Signalr.Typescript.Hubs
+{
+    public class ChatHub : Hub
+    {
+        public void Send( string name, string message )
+        {
+            // Call the addNewMessageToPage method to update clients.
+            Clients.All.addNewMessageToPage( name, message );
+        }
+    }
+}
